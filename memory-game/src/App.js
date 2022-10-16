f7ad2a48893d427a49a3b1000b4c31342b4a96c1
Jack O'Hara <jackohara96@gmail.com>
2022-10-16 18:02:44 -0400

    Responsive on mobile

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -7,8 +7,8 @@ function App() {
         Memory Game
       </header>
       <p>Get points by clicking on an image but don't click on any more than once!</p>
-      <h2>Score: <span id="score">0</span></h2>
       <h2>Best Score: <span id="bestScore">0</span></h2>
+      <h2>Score: <span id="score">0</span></h2>
       <div id="cards">
         <div class="card"></div>
         <div class="card"></div>

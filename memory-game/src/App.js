746eb5efa36e6032fd8381fe3021b50b2dea6818
Jack O'Hara <jackohara96@gmail.com>
2022-10-17 20:03:32 -0400

    Colors

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -6,14 +6,21 @@ function App() {
   const [bestScore, setBestScore] = useState(0);
   const [guesses, setGuesses] = useState([]);
   
-  const makeGuess = () => {
-   
-    setGuesses(current => [...current, 'Carl']);
-    
+  const makeGuess = (event) => {
+   console.log(event.currentTarget.className);
+   let divClass = event.currentTarget.className;
+    setGuesses(current => [...current, divClass]);
+    //loop through guesses array
+    //if the current guess class equals a class/element in the previousclass array
+    //score is set to 0
+    //can use guesses as a variable array
   }
 useEffect(() => {
-    console.log(guesses);
-  }) 
+  console.log(guesses);
+    console.log(typeof guesses);
+    
+  });
+   
   
   return (
     <div className="App">
@@ -24,18 +31,18 @@ useEffect(() => {
       <h2>Best Score: <span id="bestScore">0</span></h2>
       <h2>Score: <span id="score">0</span></h2>
       <div id="cards">
-        <div className="card" value="green" onClick={makeGuess}></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
-        <div className="card"></div>
+        <div className="card green" value="green" onClick={makeGuess}></div>
+        <div className="card green"></div>
+        <div className="card blue"></div>
+        <div className="card blue"></div>
+        <div className="card yellow"></div>
+        <div className="card yellow"></div>
+        <div className="card purple"></div>
+        <div className="card purple"></div>
+        <div className="card orange"></div>
+        <div className="card orange"></div>
+        <div className="card pink"></div>
+        <div className="card pink"></div>
       </div>
     </div>
   );
